feb7e397a8ab7fef9ed22037ebb96399864e39bf
cezar <79227201+FrentescuCezar@users.noreply.github.com>
2022-03-10 17:58:47 +0200

    Updated LectureHall and ComputerLab to have javadoc and getters for their attributes

--- Laborator 2/src/Homework/ComputerLab.java
+++ Laborator 2/src/Homework/ComputerLab.java
@@ -1,12 +1,18 @@
 package Homework;
 
+/**
+ * It's an extension for RoomH, it adds the String variable operatingSystem.
+ */
 public class ComputerLab extends RoomH{
     private String operatingSystem;
 
     public ComputerLab(String name, int capacity, String operatingSystem) {
         super(name, capacity);
         this.operatingSystem = operatingSystem;
+    }
 
+    public String getOperatingSystem() {
+        return operatingSystem;
     }
 }
 
--- Laborator 2/src/Homework/LectureHall.java
+++ Laborator 2/src/Homework/LectureHall.java
@@ -1,11 +1,18 @@
 package Homework;
 
+/**
+ * It's an extension for RoomH, it adds the boolean variable hasVideoProjector.
+ */
 public class LectureHall extends RoomH {
     private boolean hasVideoProjector;
 
     public LectureHall(String name, int capacity, boolean hasVideoProjector) {
         super(name, capacity);
         this.hasVideoProjector = hasVideoProjector;
+    }
+
+    public boolean hasVideoProjector() {
+        return hasVideoProjector;
     }
 }
 
